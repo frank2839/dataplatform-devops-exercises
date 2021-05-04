57ec90d9715d4c7fddc2216bfa849ea7aa6b5fed
Frank Huang <frank.huang@aimco.ca>
2021-05-04 15:44:39 -0600

    May 4 to add previous exercise

--- Module3-Exercise4/M3-Exercise4-Tests/Reports_StateYearReservationCount_Tests.class.sql
+++ Module3-Exercise4/M3-Exercise4-Tests/Reports_StateYearReservationCount_Tests.class.sql
@@ -1,2 +1,9 @@
 EXEC tSQLt.NewTestClass 'Reports_StateYearReservationCount_Tests';
 GO
+
+multiple hotels with differing reservation counts
+multiple hotels with no reservations
+no rows if no hotels
+one additional row for all erservations with null hotelid
+single hotel with no reservation
+
